bd12e8d74c0ce1754db7c94ed9c1c3832613a847
esawan <41877920+esawan@users.noreply.github.com>
2020-04-16 19:29:31 -0700

    Uploading from leetcode!

---
+++ validParentheses.js
@@ -0,0 +1,31 @@
+/**
+ * Algorithm practice in JavaScript!
+ * "Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+ * An input string is valid if:
+ *    - Open brackets must be closed by the same type of brackets.
+ *    - Open brackets must be closed in the correct order.
+ * Note that an empty string is also considered valid."
+ */
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+	let guide = {'(': ')', '{': '}', '[': ']'}; 
+  let check = [];
+
+	for (let letter of s) {
+        if (letter in guide) {
+            check.push(letter);
+        }
+	      else if (guide[check.pop()] !== letter) {
+            return false;
+        }
+	}
+    
+    if (check.length !== 0) {
+        return false;
+    }
+    return true;
+}
